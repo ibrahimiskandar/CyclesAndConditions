27bc0be37b2b487a8cf80bc1499fc535f1d9098f
ibrahimiskandar <100687451+ibrahimiskandar@users.noreply.github.com>
2022-10-06 06:28:37 +0400

    Add files via upload

---
+++ Conditions/Conditions/Program.cs
@@ -0,0 +1,286 @@
+﻿
+
+
+
+/*-----1.User gives 2 numbers a and b. If a>b then output a+b, if a=b then output a*b, if a<b then a-b.-----*/
+////user gives 2 number a and b. If a>b then output a+b, if a=b then output a*b, if a<b then a-b.
+//Console.WriteLine("enter a : ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(" enter b : ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//if (a > b)
+//{
+//    Console.WriteLine(a + b);
+//}
+//else if (a == b)
+//{
+//    Console.WriteLine(a * b);
+//}
+//else
+//{
+//    Console.WriteLine(a - b);
+//}
+
+
+
+/*-----2.User gives 2 numbers x and y. Find out coordinate quarter of point with location (x,y).-----*/
+////user gives 2 number x and y. Find out coordinate quarter of point with location ( x, y )
+//Console.WriteLine("enter x : ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(" enter y : ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//static void Quarter(int x, int y)
+//{
+//    if (x > y)
+//    {
+//        (y, x) = (x, y);
+//    }
+//    while (x == y)
+//    {
+//        Console.Clear();
+//        Console.WriteLine(" enter numbers again ");
+//        x = Convert.ToInt32(Console.ReadLine());
+//        y = Convert.ToInt32(Console.ReadLine());
+
+//    }
+//    Console.WriteLine((1.0 * (y - x) / 4) + x);
+//}
+//Quarter(x, y);
+
+
+
+
+
+/*-----3.User gives 3 numbers a, b, c. Output them in the ascending order. -----*/
+//Console.WriteLine("Please write 3 numbers:");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+//int c = Convert.ToInt32(Console.ReadLine());
+
+//if (a > b && a > c)
+//{
+//    if (b > c)
+//    {
+//        Console.WriteLine($"ascending order:{a} {b} {c}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"ascending order:{a} {c} {b}");
+//    }
+//}
+//if (b > a && b > c)
+//{
+//    if (a > c)
+//    {
+//        Console.WriteLine($"ascending order:{b} {a} {c}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"ascending order:{b} {c} {a}");
+//    }
+//}
+//if (c > a && c > b)
+//{
+//    if (a > b)
+//    {
+//        Console.WriteLine($"ascending order:{c} {a} {b}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"ascending order:{c} {b} {a}");
+//    }
+//}
+
+
+
+
+/*-----4.User gives 3 numbers a, b, c. Find out solution of quadratic equation ax^2+bx+c=0. -----*/
+//Console.WriteLine("enter a : ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter b : ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter c : ");
+//int c = Convert.ToInt32(Console.ReadLine());
+//static void solutionQE(int a, int b, int c)
+//{
+//    int D = b * b - 4 * a * c;
+//    if (D > 0)
+//    {
+//        double x1 = 1.0 * (-b + Math.Sqrt(D)) / 2 * a;
+//        double x2 = 1.0 * (-b - Math.Sqrt(D)) / 2 * a;
+//        Console.WriteLine($"x1={x1}, x2= {x2}");
+
+//    }
+//    else if (D == 0)
+//    {
+//        double x1 = -b / 2 * a;
+//        Console.WriteLine($"x1={x1}");
+//    }
+//    else
+//    {
+//        Console.WriteLine(" there is no roots ");
+//    }
+//}
+//solutionQE(a, b, c);
+
+
+
+
+
+
+
+
+/*-----5.User gives 2-digit number. Output the world according to this number. F.e. : 13 – thirteen, 25 – twenty five and so on. -----*/
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//static void word(int number)
+//{    
+//    //Console.WriteLine("Input number : ");
+//    string[] ones = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+//    string[] tens = { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+//    string word = "";
+//    if (number < 20)
+//    {
+//        Console.WriteLine(ones[number]);
+//    }
+//    else
+//    {
+//        int last = number % 10;
+//        if (last > 0)
+//        {
+//            word += tens[number / 10] + " " + ones[last];
+//        }
+//        else
+//        {
+//            word += tens[number / 10];
+//        }
+//        Console.WriteLine(word);
+//    }
+//}
+
+//word(number);
+
+
+
+
+
+/*-----6.User gives 3 numbers x, y, r. Find out if point with location (x,y) is inside the circle with radius r?-----*/
+//Console.WriteLine("enter x : ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter y : ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter r : ");
+//int r = Convert.ToInt32(Console.ReadLine());
+//static void distance(int x, int y, int r)
+//{
+//    int formula = x * x + y * y ;
+//    if (formula < r * r)
+//    {
+//        Console.WriteLine(" inside ");
+//    }
+//    else if (formula == r * r)
+//    {
+//        Console.WriteLine(" onside");
+//    }
+//    else
+//    {
+//        Console.WriteLine(" outside ");
+//    }
+//}
+//distance(x, y, r);
+
+
+
+
+
+
+/*-----7.Write a program with 4 different options. Below you’ll see what should happen if user will choose one of those options:-----*/
+/*-----a.Take a 3-digit number as input, and say if square of this number equals sum of all it’s digits to the power of 3.-----*/
+//int number = int.Parse(Console.ReadLine()); //123
+//int sum = 0, remain;
+//if (number > 99 && number < 1000)
+//{
+//    while (number != 0)
+//    {
+//        remain = number % 10;
+//        sum += remain;
+//        number = number / 10;
+//    }
+//    if (number * number == sum * sum * sum)
+//    {
+//        Console.WriteLine("It's equal");
+//    }
+//    else
+//    {
+//        Console.WriteLine("It's not equal");
+//    }
+//}
+/*-----i.Take a 4-digit number and say if sum of first two digits equals sum of last 2 digits.-----*/
+// Console.WriteLine("please enter a 4-digit number:");
+//int number1 = int.Parse(Console.ReadLine()); //1234
+//if (number1 > 999 && number1 < 10000)
+//{
+//    int sum1 = 0, sum2 = 0, remain1;
+//    for (int i = 1; i < 3; i++)
+//    {
+//        remain1 = number1 % 10;
+//        sum2 += remain1;
+//        number1 /= 10;
+//    }
+//    remain1 = number1 % 10;
+//    sum1 = remain1 + (number1 / 10);
+//    if (sum1 == sum2)
+//    {
+//        Console.WriteLine("ıt's equal");
+//    }
+//    else
+//    {
+//        Console.WriteLine("ıt's not equal");
+//    }
+//}
+/*-----b.Take a 3-digit number as input. Say if there any 2 similar digits in it.-----*/
+//Console.WriteLine("please enter a 3-digit number:");
+//int number2 = int.Parse(Console.ReadLine()); //232
+//if (number2 > 99 && number2 < 1000)
+//{
+//    int temp = number2;
+//    int remain2 = number2 % 10;
+//    int remain1 = number2 / 100;
+//    bool istrue = false;
+//    while (number2 != 0)
+//    {
+//        number2 = number2 / 10;
+//        if (remain2 == number2 % 10)
+//        {
+//            istrue = true;
+//        }
+//        remain2 = number2 % 10;
+//    }
+//    if (istrue || remain1 == temp %10)
+//        Console.WriteLine("there are similar digits");
+//    }
+//    else
+//    {
+//        Console.WriteLine("there are not similar digits");
+//    }
+/*-----c.Take a float number. Say if first 3 digits after comma contains 0.-----*/
+//Console.WriteLine("Please enter a float number:");
+//float number = float.Parse(Console.ReadLine()); //1,10233 
+//int numberInt = Convert.ToInt32(number);
+//int fractionInt = Convert.ToInt32((number - numberInt) * 1000);
+//bool isTrue = false;
+//while (fractionInt>0)
+//{
+//	if (fractionInt%10==0)
+//	{
+//		Console.WriteLine("contains 0");
+//		isTrue = true;
+//		break;
+//	}
+//	fractionInt /= 10;
+//}
+//if (isTrue==false)
+//{
+//	Console.WriteLine("there is no 0");
+//}
+
---
+++ Cycles/Cycles/Program.cs
@@ -0,0 +1,640 @@
+﻿/*-------------------------1.User gives 2 numbers a, b. Output a to the power of b (not using Math.pow).-----------------*/
+
+//Console.WriteLine("Input number:");
+//int variable = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Input power:");
+//int power = Convert.ToInt32(Console.ReadLine());
+
+
+//static void Power(int a, int b)
+//{
+//    int power = 0;
+
+//    for (int i = 1; i <= b; i++)
+//    {
+//        int multiple = a * a;
+
+//        power += multiple;
+//    }
+//    Console.WriteLine(power);
+//}
+//Power(variable, power);
+
+
+/*-------------------------2.User gives 1 number a. Output amount of number from 1 to 1000 that have no reminder when divided by a.-----------------*/
+
+//Console.WriteLine("Input number:");
+//int variable = Convert.ToInt32(Console.ReadLine());
+
+//static void checkRemainder(int a)
+//{
+//    int count = 0;
+//    for (int i = 1; i <= 1000; i++)
+//    {
+//        int remainder = i % a;
+//        if (remainder == 0)
+//        {
+//            count++;
+//        }
+//    }
+//    Console.WriteLine(count);
+//}
+
+//checkRemainder(variable);
+
+/*-------------------------3.User gives 1 number a. Output all positive numbers whose square will be lesser than a-----------------*/
+
+//Console.WriteLine("Input number:");
+//int variable = Convert.ToInt32(Console.ReadLine());
+//static void checkSquare(int a)
+//{
+//    for (int i = 1; i * i < a; i++)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+//checkSquare(variable);
+
+/*-------------------------4.User gives 1 number a. Output largest divisor to that number.-----------------*/
+
+//Console.WriteLine("Input the number:");
+//int variable = Convert.ToInt32(Console.ReadLine());
+//static void maxDivisor(int a)
+//{
+//    int maxDivisor = 1;
+//    for (int i = 2; i < a; i++)
+//    {
+//        if (a % i == 0)
+//        {
+//            maxDivisor = a / i;
+//            break;
+//        }
+//    }
+//    Console.WriteLine(maxDivisor);
+//}
+//maxDivisor(variable);
+
+
+/*-------------------------5.User gives 2 numbers a,b. Output sum of all numbers from range a to b that have no reminder when divided by 7. (there can be situation when b lesser than a)-----------------*/
+
+//Console.WriteLine("Input the First number:");
+//int start = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Input the Second number:");
+//int end = Convert.ToInt32(Console.ReadLine());
+
+//static void checkRemainderSeven(int a, int b)
+//{
+//    int sum = 0;
+
+//    if (a > b)
+//    {
+//        (a,b)=(b,a)
+//    }
+//    for (int i = a; i < b; i++)
+//    {
+//        int remainder = i % 7;
+//        if (remainder == 0)
+//        {
+//            sum += i;
+//        }
+//    }
+//    Console.WriteLine(sum);
+//}
+//checkRemainderSeven(start, end);
+
+
+/*-------------------------6.User gives 1 number a. Output a-th number of Fibonacci’s series. (In Fibonacci series every next number is sum of previous two. F.e. : 1 1 2 3 5 8 13 21 …)-----------------*/
+
+//Console.WriteLine("Input number:");
+//int fibonacci = Convert.ToInt32(Console.ReadLine());
+
+//static void Fibonacci(int a)
+//{
+//    int n1 = 0;
+//    int n2 = 1;
+//    int sum = 0;
+//    for (int i = 2; i <= a; i++)
+//    {
+//        sum = n1 + n2;
+//        n1 = n2;
+//        n2 = sum;
+//    }
+//    Console.WriteLine(sum);
+//}
+//Fibonacci(fibonacci);
+
+/*-------------------------7.User gives 2 numbers. Find out their greatest common divisor using Euclid’s algorithm-----------------*/
+
+
+//Console.WriteLine("birinci Input number:");
+//int first = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("ikinci Input number:");
+//int second = Convert.ToInt32(Console.ReadLine());
+
+//static void GCD(int first, int second)
+//{
+//    int m = first;
+//    int n = second;
+//    if (n>m)
+//    {
+//        (m,n)=(n,m);
+//    }
+//    while (first != 0 && second != 0)
+//    {
+//        if (first > second)
+//        {
+//            first %= second;
+//        }
+//        else
+//        {
+//            second %= first;
+//        }
+//    }
+
+//    if (first == 0)
+//    {
+//        Console.WriteLine($"GCD({m},{n}) is {second}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"GCD({m},{n}) is {first}");
+//    }
+//}
+//GCD(first, second);
+
+
+/*-------------------------8.User gives a positive number that is power of another number to 3. Find out that number using half division method. (F.e. : 8->2 )-----------------*/
+
+
+//Console.WriteLine("Define a: ");
+//double a = double.Parse(Console.ReadLine());
+//int b = Convert.ToInt32(a / 2);
+
+//static void Root(double a, int b)
+//{
+//    int result = 0;
+//    while (Math.Pow(b, 3) > a)
+//    {
+//        b = Convert.ToInt32(b / 2);
+//    }
+//    for (int i = b; i * i * i <= a; i++)
+//    {
+//        result = i;
+//    }
+//    if (Math.Pow(result,3)==a)
+//    {
+//        Console.WriteLine(result);
+//    }
+//    else
+//    {
+//        Console.WriteLine($"There is no number for {a}'s 3th root");
+//    }
+
+//}
+//Root(a, b);
+/*-------------------------9.User gives 1 number. Find out amount of odd digits in this number.-----------------*/
+
+//Console.WriteLine("Input number:");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//static void checkOdd(int number)
+//{
+//    int odd;
+//    while(number > 0)
+//    {
+//        int last = number % 10;
+//        odd = last % 2;
+
+//        if (odd == 1)
+//        {
+//            Console.WriteLine(last);
+//        }
+
+//        number /= 10;
+//    }
+//}
+//checkOdd(number);
+
+/*-------------------------10.User gives 1 number. Find out mirrored number to that one. F.e. : 1234 -> 4321. -----------------*/
+
+//Console.WriteLine("Input number:");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//static void Mirrored(int number)
+//{
+//    int mirrored = 0;
+
+//    while (number > 0)
+//    {
+//        int last = number % 10;
+
+//        mirrored = mirrored*10 + last;
+
+//        number /= 10;
+//    }
+//    Console.WriteLine(mirrored);
+//}
+//Mirrored(number);
+
+/*-------------------------11.User gives 1 number a. Find out every number in range 1 to a whose sum of even digits bigger than odd. -----------------*/
+
+//Console.WriteLine("Input number:");
+//int a = Convert.ToInt32(Console.ReadLine());
+
+//static void compareSum(int a)
+//{
+
+//    for (int i = 10; i < a; i++)
+//    {
+//        int sumEven = 0;
+//        int sumOdd = 0;
+//        int temp = i;
+
+//        while (temp != 0)
+//        {
+//            int last = temp % 10;
+
+//            if (last % 2 == 0)
+//            {
+//                sumEven += last;
+//            }
+//            else
+//            {
+//                sumOdd += last;
+//            }
+//            temp /= 10;
+//        }
+
+//        if (sumEven > sumOdd)
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+//}
+//compareSum(a);
+
+/*-------------------------12.User gives 2 numbers. Find out if there are similar digits in those numbers.-----------------*/
+
+//Console.WriteLine("Enter the first number:");
+//int first = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter the second number:");
+//int second = Convert.ToInt32(Console.ReadLine());
+
+//static void compareSame(int a, int b)
+//{
+
+//    bool result =false;
+//    int temp = a;
+
+//    for (int i = 0; i < a; i++)
+//    { 
+//        int digitA = a % 10;
+//        int digitB = b % 10;
+//        if (digitA == digitB)
+//        {
+//            result = true;
+//            break;
+//        }
+//        if (temp!> 0)
+//        {
+//            temp = a;
+//            b /= 10;
+//            i = 0;
+//        }
+//        temp /= 10;
+//        if (b==0)
+//        {
+//            break;
+//        }
+//    }
+//    Console.WriteLine(result);
+//}
+//compareSame(first, second);
+
+/*-------------------------12.User gives 2 numbers. Find out if there are similar digits in those numbers. -----------------*/
+
+//Console.WriteLine("Input number:");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//static void Formula(int a)
+//{
+//    double sum = 0;
+//    for (int i = 1; i <= a; i++)
+//    {
+//        sum += 1+ (1.0 / (i * i));
+//    }
+//    Console.WriteLine(sum);
+//}
+//Formula(n);
+
+/*-------------------------14.User gives х, and positive n. Calculate: -----------------*/
+
+//Console.WriteLine("Input first number:");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input second number:");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//static void Formula(int x,int n)
+//{
+//    int denominator = 1;
+//    double sum = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        denominator *= (x + i);
+//        sum += 1.0 / denominator;
+//    }
+//    Console.WriteLine(sum);
+//}
+// Formula( x,n)
+//static void formulaFactorial(int x,int n)
+//{
+//    int factorial = 1;
+//    double sum = 0;
+//    for (int i = 1; i <= n; i++)
+//    {
+//        double numerator = Math.Pow(x, i);
+//        factorial *= i;
+//        sum += numerator / factorial;
+//    }
+//    Console.WriteLine(sum);
+//}
+//formulaFactorial(x,n);
+
+/*-------------------------15.User gives positive n. Calculate-----------------*/
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//static void Multiplation(int n)
+//{
+//    double multiplation = 1;
+//    int factorial = 1;
+//    for (int i = 1; i <= n; i++)
+//    {
+//        factorial *= i;
+//        multiplation *= 2+(1.0/ factorial);
+//    }
+//    Console.WriteLine(multiplation);
+//}
+//Multiplation(n);
+
+//static void Sum(int n)
+//{
+//    double sum = 0;
+//    int factorial = 1;
+//    for (int i = 1; i <= n; i++)
+//    {
+//        factorial *= i;
+//        sum *= (i + 1.0) / factorial;
+//    }
+//    Console.WriteLine(sum);
+//}
+//Sum(n);
+/*-------------------------16.Calculate the approximate value of an infinite sum. After equls sign you see it’s exact value for you to compare with your result.:-----------------*/
+//Console.WriteLine("Input number d:");
+//int d = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input number x:");
+//int x = Convert.ToInt32(Console.ReadLine());
+
+//static void checkInaccuracy(int n)
+//{
+//    double result = Math.Pow(Math.PI, 2) / 6;
+//    double sum = 0;
+//    for (int i = 1; i <= n; i++)
+//    {
+//        sum += 1.0 / (i * i);
+//    }
+
+//    Console.WriteLine($"Inaccuracy in this calculation is {result - sum}");
+//}
+//checkInaccuracy(n);
+
+//static void checkInaccuracy(int n)
+//{
+//    double result = 3.0 / 4;
+//    double sum = 0;
+//    for (int i = 1; i <= n; i++)
+//    {
+//        sum += 1.0 / (i * (i+2));
+//    }
+
+//    Console.WriteLine($"Inaccuracy in this calculation is {result - sum}");
+//}
+//checkInaccuracy(n);
+
+//static void checkInaccuracy(int d,int x)
+//{
+//    double result = Math.Pow(Math.E, x) ;
+//    int factorial = 1;
+//    double sum = 1;
+//    for (int i = 1; i <= d; i++)
+//    {
+//        factorial *= i;
+//        sum += Math.Pow(x,i) / factorial;
+//    }   
+
+//    Console.WriteLine($"Inaccuracy in this calculation is {result - sum}");
+//}
+//checkInaccuracy(d,x);
+
+/*-------------------------17.Find out all 2-digits numbers, whose digit’s sum doesn’t change after multiplying this number by 2,3,4,5,6,7,8,9.-----------------*/
+
+
+//static void checkChange() { 
+//    for (int i = 10; i < 100; i++)
+//    {
+//        int sum = Sum(i);
+
+//        for (int j = 2; j < 10; j++)
+//        {
+//            int multiple = i * j;
+//            if (sum==Sum(multiple))
+//            {
+//                Console.WriteLine($"{i}--{j}");
+//            }
+//        }
+//    }
+//}
+//static int Sum(int n)
+//{
+//    int sum = 0;
+//    while (n>0)
+//    {
+//        sum += n % 10;
+//        n /= 10;
+//    }
+//    return sum;
+//}
+//checkChange();
+
+
+/*-------------------------18.Find out every 3-digit number that can be represented as sum of all its digit’s factorials.-----------------*/
+//static void checkChange()
+//{
+//    for (int i = 100; i < 1000; i++)
+//    {
+//        if (i == digitFactorialSum(i))
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+//}
+
+//static int digitFactorialSum(int n)
+//{
+//    int factorial = 1;
+//    int factorialSum = 0;
+//    while (n>0)
+//    {
+//        int digit = n % 10;
+//        for (int i = 1; i <= digit; i++)
+//        {
+//            factorial *= i;
+//        }
+//        factorialSum += factorial;
+//        factorial = 1;
+//        n /= 10;
+//    }
+//    Console.WriteLine(factorialSum);
+//    return factorialSum;
+//}
+
+//checkChange();
+
+/*-------------------------19.Is it possible to represent given number as sum of two positive numbers powered to 2? -----------------*/
+//Console.WriteLine("Input n:");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//static void checkSquare(int n)
+//{
+//	bool isTrue = false;
+//	for (int i = 1; i*i < n; i++)
+//	{
+//		for (int j = i; j*j < n; j++)
+//		{
+//			if ((i*i)+(j*j)==n)
+//			{
+//                Console.WriteLine($"It is possible to represet given number as sum of {i} and {j} powered to 2");
+//                isTrue = true;
+//			}
+//		}
+//		if (isTrue)
+//		{
+//			break;
+//        }
+//    }
+//}
+
+//checkSquare(n);
+
+/*-------------------------20.Find out is given number perfect? I.e. number is equal to sum of all it’s positive divisors excepting the number by it’s own. -----------------*/
+
+//Console.WriteLine("Input n:");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//if (n==sumOfDivisors(n))
+//{
+//	Console.WriteLine("It is perfect Number");
+//}
+//else
+//{
+//    Console.WriteLine("It is not a perfect Number");
+//}
+//static int sumOfDivisors(int n)
+//{
+//    int sum = 1;
+//	for (int i = 2; i <= n/2; i++)
+//	{
+//		if (n%i==0)
+//		{
+//			sum += i;
+//		}
+//	}
+//	return sum;
+//}
+
+/*-------------------------21.User gives one positive number k. Find out k-th number of series: 1234567891011121314… which have every positive numbers one by one. -----------------*/
+
+
+//Console.WriteLine("Input k:");
+//int k = Convert.ToInt32(Console.ReadLine());
+
+
+//static void findDigit(int k)
+//{
+//    /*-------------------------There is for optimizing  loops iteration count -----------------*/
+//    if (k % 2 == 1 && k > 9)
+//    {
+//        k--;
+//    }
+//    if (k > 10)
+//    {
+//        k = 10 + (Convert.ToInt32((k - Math.Pow(10, (numberCount(k) - 1))) / 2));
+//    }
+//    /*-------------------------There is for optimizing loops iteration count -----------------*/
+//    long number = 1;
+//    for (int i = 2; i <= k; i++)
+//    {
+//        number *= Convert.ToInt64(Math.Pow(10, (numberCount(i))));
+//        number += i;
+
+//    }
+//    Console.WriteLine(number);
+//    if (k % 2 == 0)
+//    {
+//        Console.WriteLine(number / 10 % 10);
+//    }
+//    else
+//    {
+//        Console.WriteLine(number % 10);
+//    }
+//}
+//static int numberCount(int n)
+//{
+//    int count = 0;
+//    while (n > 0)
+//    {
+//        count++;
+//        n /= 10;
+//    }
+//    return count;
+//}
+//findDigit(k);
+
+/*-------------------------22.User gives one positive number k. Find out k-th number of series: 149162536… which have every positive numbers one by one powered by 2. -----------------*/
+
+
+//Console.WriteLine("Input k:");
+//int k = Convert.ToInt32(Console.ReadLine());
+
+//static void findDigit(int k)
+//{
+//    long number = 1;
+//    for (int i = 2; i <= k; i++)
+//    {
+//        number *= Convert.ToInt64(Math.Pow(10, (numberCount(i*i))));
+//        number += (i*i);
+
+//    }
+//    Console.WriteLine(number);
+//    if (k % 2 == 0)
+//    {
+//        Console.WriteLine(number / 10 % 10);
+//    }
+//    else
+//    {
+//        Console.WriteLine(number % 10);
+//    }
+//}
+//static int numberCount(int n)
+//{
+//    int count = 0;
+//    while (n > 0)
+//    {
+//        count++;
+//        n /= 10;
+//    }
+//    return count;
+//}
+//findDigit(k);
